f294de7e84ff20e6cfb21b320dd14d920a17c274
Marie Joyce Tio <joycetio@Joyces-MacBook-Pro.local>
2017-05-04 20:31:52 -0700

    burgers db & content added

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Chicken Bacon Burger", false); 
+INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Plain Burger", false); 
+INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("In-N-Out Double Cheeseburger", true);
