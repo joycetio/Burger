33642a14e690de28402e9f22c15aac0ab7169955
Marie Joyce Tio <joycetio@Joyces-MacBook-Pro.local>
2017-05-04 20:01:03 -0700

    server.js done

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
-var express = require('express'); 
+//dependencies 
+var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+
+//import routes and give the server access to them.
+var routes = require('.controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+//express setup 
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//sets up the Express app to handle data parsing
+app.use(express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+
+//override w POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+//set handlebars 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+};
