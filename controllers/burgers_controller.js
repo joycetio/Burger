a4c08c22ac44faba1f17d3203dff8b44b5c5a04b
Marie Joyce Tio <joycetio@Joyces-MacBook-Pro.local>
2017-05-05 10:28:12 -0700

    controllers done

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,35 @@
+var express = require("express"); 
+var router = express.Router(); 
+var burger = require("../models/burger.js"); 
+
+//create all our routes and set up logic within those routes where required. 
+router.get('/', function(req, res) {
+	burger.selectAll(function(data) {
+		var handlebarsObj = {
+			burgers: data
+		}; 
+		console.log("handlebarsObj: ", handlebarsObj); 
+		res.render("index", handlebarsObj);
+	});
+}); 
+
+router.post("/", function(req, res) {
+	burger.insertOne(req.body.burgerUserInput, function() {
+		res.redirect("/");
+	});
+});
+
+router.put("/:id", function(req, res) {
+	var condition = "id = " + req.params.id; 
+
+	console.log("condition: ", condition); 
+
+	burger.updateOne({
+		DEVOURED: req.body.DEVOURED
+	}, condition, function() {
+		res.redirect("/");
+	});
+}); 
+
+//exports for server.js
+module.exports = router; 
